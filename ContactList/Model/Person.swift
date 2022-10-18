4882489466210991443ec827ad6797d8a3deec5f
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-10-18 16:20:10 +0200

    upload persons list

--- ContactList/Model/Person.swift
+++ ContactList/Model/Person.swift
@@ -5,4 +5,68 @@
 //  Created by Roman on 18.10.2022.
 //
 
-import Foundation
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var title: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPersonList() -> [Person] {
+        [
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? ""),
+            Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                email: emails.randomElement() ?? "",
+                phoneNumber: phoneNumbers.randomElement() ?? "")
+        ]
+    }
+}
--- ContactList/ViewControllers/ContactTableViewController.swift
+++ ContactList/ViewControllers/ContactTableViewController.swift
@@ -9,22 +9,27 @@ import UIKit
 
 class ContactTableViewController: UITableViewController {
     
+    private var personList = Person.getPersonList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.rowHeight = 40
     }
 }
 
 // MARK: - UITableViewDataSource
 extension ContactTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return personList.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "persons", for: indexPath)
+        let person = personList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = person.title
+        cell.contentConfiguration = content
+        
         return cell
     }
 
