97a391304be8c4b3174c80ae23b37e0d68a1c248
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-10-18 15:36:34 +0200

    create view controllers and mvc

---
+++ ContactList/ViewControllers/ContactTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ContactTableViewController.swift
+//  ContactList
+//
+//  Created by Roman on 18.10.2022.
+//
+
+import UIKit
+
+class ContactTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+
+        return cell
+    }
+
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
